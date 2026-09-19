8eb4aa3d2e696a59669b8ad63ed708e2396764c7
Bleidys Larios <lariosbleidys@gmail.com>
2026-09-19 12:16:12 -0500

    Retirar backend Django y blueprint de Render

--- backend/export/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('pdf/', views.export_pdf, name='export-pdf'),
-    path('csv/', views.export_csv, name='export-csv'),
-]
